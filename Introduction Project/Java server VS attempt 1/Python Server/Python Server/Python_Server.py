e845c7c6d42f4e4fc532dab1bb987545fc083549
Valentin <i432543@fhict.nl>
2019-11-01 02:51:42 +0100

    Python code

--- Introduction Project/Java server VS attempt 1/Python Server/Python Server/Python_Server.py
+++ Introduction Project/Java server VS attempt 1/Python Server/Python Server/Python_Server.py
@@ -4,7 +4,7 @@ import socketserver
 PORT = 42069
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(( "145.93.62.204", PORT), Handler) as httpd:
+with socketserver.TCPServer(( "10.28.109.112", PORT), Handler) as httpd:
     #httpd = socketserver.TCPServer((“”, PORT), Handler) or use this
     print("Serving at port: ", PORT)
     httpd.serve_forever()
