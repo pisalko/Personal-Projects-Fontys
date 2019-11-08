d5050305a4398e806ac75677fb2705fabd7b36c0
Valentin <432543@student.fontys.nl>
2019-11-08 04:56:19 +0100

    embedded finished

--- embedded systems/arduino projects/VS projects/Nursing Bed Alarm/Form1.cs
+++ embedded systems/arduino projects/VS projects/Nursing Bed Alarm/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Nursing_Bed_Alarm
@@ -13,6 +14,7 @@ namespace Nursing_Bed_Alarm
         String textInPort = "";
         bool alarmSounded;
         string timeOnly;
+        int temperature;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -23,13 +25,11 @@ namespace Nursing_Bed_Alarm
         bool once = true;
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
-            //if (modeCheck == 1)
-            //{
-                DateTime dt = DateTime.Now;
+           
             if (modeCheck == '1')
             {
-                if(once)
+                DateTime dt = DateTime.Now;
+                if (once)
                 {
                     serialPort1.WriteLine("t:" + timeOnly);
                     once = false;
@@ -42,50 +42,74 @@ namespace Nursing_Bed_Alarm
                     serialPort1.WriteLine("t:" + timeOnly);
                     
                 }
+                label2.Text = "Switch to mode 2 for room temperature.";
             }
-            //}
+            else if (modeCheck == '2')
+            {
+                label2.Visible = true;
+                double temperatureFl = Convert.ToDouble(temperature) / 100;
+                    label2.Text = "Room temperature is: " + temperatureFl.ToString() + " degrees Celsius";
+            }
+            else if (modeCheck == '3')
+            {
+                label2.Text = "Switch to mode 2 for room temperature.";
+            }
+
+            if (modeCheck != '1')
+                once = true;
+
+            if(alarmSounded)
+            {
+                pictureBox1.BackColor = Color.Red;
+            }
+            
         }
 
         private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
-            //modeCheck = Convert.ToChar(serialPort1.ReadChar());
+            
+
             textInPort = serialPort1.ReadLine();
             textInPort.Trim();
             textInPort = textInPort.Replace("\r\n", "").Replace("\r", "").Replace("\n", "");
+            
 
-            /*if (textInPort.StartsWith("s"))
+            if(textInPort == "a")
             {
-                MessageBox.Show(textInPort.Substring(1, textInPort.Length - 1));
-            }*/
 
-            switch (textInPort)
-            {
-                case "a":
-                    alarmSounded = true;
-                    break;
-                default:
-                    alarmSounded = false;
-                    break;
-                    
-
-                case "1":
-                    modeCheck = '1';
-                    break;
-
-                case "2":
-                    modeCheck = '2';
-                    break;
-
-                case "3":
-                    modeCheck = '3';
-                    break;
+                alarmSounded = true;
             }
+            else
+            {
+                alarmSounded = false;
+                switch (textInPort)
+                {
+                    case "1":
+                        modeCheck = '1';
+                        break;
 
+                    case "2":
+                        modeCheck = '2';
+                        break;
+
+                    case "3":
+                        modeCheck = '3';
+                        break;
+
+
+                }
+            }
+                if (textInPort.StartsWith("z"))
+                {
+                    temperature = Convert.ToInt32(textInPort.Substring(1));
+                }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            serialPort1.WriteLine("r");
+            pictureBox1.BackColor = Color.Transparent;
+            alarmSounded = false;
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
@@ -96,6 +120,21 @@ namespace Nursing_Bed_Alarm
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox1_BackColorChanged(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            if (pictureBox1.BackColor == Color.Red)
+            { 
+            string time = dt.ToString("HH:mm");
+            listBox1.Items.Add(time + "  Alarm Sounded!");
+            } 
+            else if (pictureBox1.BackColor == Color.Transparent)
+            {
+                string time = dt.ToString("HH:mm");
+                listBox1.Items.Add(time + "  Alarm Reset!");
+            }
         }
     }
 }
